b0107b8d1c2d2ab4a66ee87b8a99e1591a8d7e7a
SIEA <69807609+DrVosss@users.noreply.github.com>
2024-06-23 19:57:03 +0200

    registering all commands

--- src/main/java/dev/siea/barista/Barista.java
+++ src/main/java/dev/siea/barista/Barista.java
@@ -1,13 +1,26 @@
 package dev.siea.barista;
 
+import dev.siea.barista.commands.LetMeGoogleThatForYouCommand;
+import dev.siea.barista.commands.TryAndSeeCommand;
+import dev.siea.barista.commands.administration.BanCommand;
+import dev.siea.barista.commands.administration.KickCommand;
+import dev.siea.barista.commands.simple.CatbaseCommand;
+import dev.siea.barista.commands.simple.InfoCommand;
 import net.vitacraft.jmjda.JMBot;
+import net.vitacraft.jmjda.api.commands.PrimitiveCommandsManager;
 
 public class Barista {
 
 
     public Barista(String token){
         JMBot bot = new JMBot(token);
-
+        PrimitiveCommandsManager manager = bot.getPrimitiveCommandsManager();
+        manager.registerCommand(new BanCommand());
+        manager.registerCommand(new KickCommand());
+        manager.registerCommand(new CatbaseCommand());
+        manager.registerCommand(new InfoCommand());
+        manager.registerCommand(new LetMeGoogleThatForYouCommand());
+        manager.registerCommand(new TryAndSeeCommand());
     }
 
     public static void main(String[] args) {
