5366906d1fee6b2d9a6e9e1ed9d44cfb89a6f21d
SIEA <69807609+DrVosss@users.noreply.github.com>
2024-06-23 20:12:58 +0200

    Added config loading for token and new channel

--- src/main/java/dev/siea/barista/Barista.java
+++ src/main/java/dev/siea/barista/Barista.java
@@ -8,15 +8,20 @@ import dev.siea.barista.commands.simple.CatbaseCommand;
 import dev.siea.barista.commands.simple.InfoCommand;
 import dev.siea.barista.games.counting.Counting;
 import dev.siea.barista.misc.JoinMessenger;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.sharding.ShardManager;
 import net.vitacraft.jmjda.JMBot;
 import net.vitacraft.jmjda.api.commands.PrimitiveCommandsManager;
+import net.vitacraft.jmjda.api.config.ConfigUtil;
+import net.vitacraft.jmjda.api.config.Filetype;
 
 public class Barista {
 
 
-    public Barista(String token){
+    public Barista(){
+        String token = ConfigUtil.getConfig("config.yml", Filetype.YAML).getString("token");
         JMBot bot = new JMBot(token);
+        bot.setActivity(Activity.watching("You!"));
         PrimitiveCommandsManager manager = bot.getPrimitiveCommandsManager();
         manager.registerCommand(new BanCommand());
         manager.registerCommand(new KickCommand());
@@ -31,6 +36,6 @@ public class Barista {
     }
 
     public static void main(String[] args) {
-        Barista barista = new Barista("");
+        Barista barista = new Barista();
     }
 }
